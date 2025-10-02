96dba45082b6dccc550567c98719ebc710057522
PauloSpohrBR <paulosporhluz@gmail.com>
2025-10-01 21:41:28 -0300

    Reverva voo

---
+++ src/main/java/com/br/revervavoo/RevervavooApplication.java
@@ -0,0 +1,13 @@
+package com.br.revervavoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevervavooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevervavooApplication.class, args);
+	}
+
+}
